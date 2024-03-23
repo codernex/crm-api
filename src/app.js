787864c95718cc127c212895dcb85d3eb9c98665
Borhan <borhan.dev@gmail.com>
2024-03-23 13:55:12 +0600

    API

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pcl-crm-frontend-xi.vercel.app"],
+    origin: ["http://localhost:5173", "https://pcl-crm-frontend-xi.vercel.app","https://pcl-frontend-temp.vercel.app"],
     credentials: true,
   })
 );
